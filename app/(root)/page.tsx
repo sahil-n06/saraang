a038734edcd58bd8fd774e94a24b4f493fba1f2f
sahil <mdsahil622004@gmail.com>
2024-10-29 08:05:16 +0530

    photo changed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,33 +4,37 @@ import { Button } from "@/components/ui/button";
 import React from 'react';
 import Collection from '@/components/shared/Collection';
 
-
 const HeroSection = () => {
   return (
-    <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
-      <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
-        <div className="flex flex-col justify-center gap-8">
-          <h1 className="h1-bold">Saraang, Celebrate Your Passion</h1>
-          <p className="p-regular-20 md:p-regular-24">
-            Unlock your creative potential with Saraang: discover, participate, and get recognized in diverse cultural events.
-          </p>
-          <Button size="lg" asChild className="button w-full sm:w-fit">
-            <Link href="#events">
-              Explore Now
-            </Link>
-          </Button>
-        </div>
+    <>
+      <section
+        className="
+          relative py-5 md:py-10 overflow-hidden 
+          bg-[url('/assets/images/hero.png')] 
+          sm:bg-[url('/assets/images/hero.png')] 
+          bg-cover bg-center"
+        style={{ height: '100vh' }}
+      >
+        {/* Light overlay to reduce the intensity of the background image */}
+        <div className="absolute inset-0 bg-white opacity-30 z-10"></div>
 
-        <Image 
-          src="/assets/images/hero.png"
-          alt="hero"
-          width={1000}
-          height={1000}
-          className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
-        />
-      </div>
-      <Collection/>
-    </section>
+        {/* Content Wrapper */}
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0 relative z-20">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">Saraang, Celebrate Your Passion</h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Unlock your creative potential with Saraang: discover, participate, and get recognized in diverse cultural events.
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">
+                Explore Now
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </section>
+      <Collection />
+    </>
   );
 };
 
